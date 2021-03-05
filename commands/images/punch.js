985ff53a9250082e45363885225c515c5353a3d4
Glitch (buttered-successful-bull) <none>
2021-03-05 18:20:04 +0000

    🏏🐆 Checkpoint ./commands/owners/dnd.js:23585414/3199 ./commands/images/punch.js:23585414/5076

---
+++ commands/images/punch.js
@@ -0,0 +1,25 @@
+const discord = require("discord.js");
+const { Random } = require("something-random-on-discord");
+const random = new Random();
+module.exports = {
+  name: "punch",
+  category: "fun",
+  description: "Punch someone",
+
+  run: async (client, message, args) => {
+    let target = message.mentions.members.first();
+    let data = await random.getAnimeImgURL("punch");
+    let embed = new discord.MessageEmbed()
+      .setTitle("Punch")
+      .setImage(data)
+      .setColor("RED")
+      .setDescription(`${message.author} punches ${target.user}`)
+      .setFooter(
+        "Requested By : " + message.author.tag,
+        message.author.displayAvatarURL()
+      )
+      .setTimestamp((message.timestamp = Date.now()));
+
+    message.channel.send(embed);
+  }
+};
