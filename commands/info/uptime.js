0f80aa988afffc20489d4fd27a1803884645793c
Glitch (buttered-successful-bull) <none>
2021-03-09 18:08:14 +0000

    🚓☺️ Checkpoint ./commands/info/uptime.js:23585414/8909

--- commands/info/uptime.js
+++ commands/info/uptime.js
@@ -1,39 +1,36 @@
-const = require('discord.js')
+const discord = require("discord.js");
 
 module.exports = {
   name: "uptime",
-
   aliases: ["u"],
+  description: "uptime.description",
 
-  description:"uptime.description"
-
-  execute(message) {
+  run(client, message, args) {
     let seconds = Math.floor(message.client.uptime / 1000);
-
     let minutes = Math.floor(seconds / 60);
-
     let hours = Math.floor(minutes / 60);
-
     let days = Math.floor(hours / 24);
-
     seconds %= 60;
-
     minutes %= 60;
-
     hours %= 24;
 
     let embed = new discord.MessageEmbed()
-    
-    .setTitle("Bot Uptime")
-    .addField("Day's",days)
-    .addField("Hour's",hours)
-    .addField("Minute's", minutes)
-    .addField("Second's", seconds)
-    
-    return 
-    mmessage.channel.send(embed)
-      
 
-      .catch(console.error);
+      .setTitle(
+        "<:marvel_bot:814481094732415026> Bot Uptime <:marvel_bot:814481094732415026>"
+      )
+      .addField("<:marvel_arrow_right:815105698853552128> Day's", days)
+      .addField("<:marvel_arrow_right:815105698853552128> Hour's", hours)
+      .addField("<:marvel_arrow_right:815105698853552128> Minute's", minutes)
+      .addField("<:marvel_arrow_right:815105698853552128> Second's", seconds)
+      .setThumbnail(client.user.displayAvatarURL())
+      .setColor("RED")
+      .setFooter(
+        "Requested By : " + message.author.tag,
+        message.author.displayAvatarURL()
+      )
+      .setTimestamp((message.timestamp = Date.now()));
+
+    message.channel.send(embed).catch(console.error);
   }
 };
