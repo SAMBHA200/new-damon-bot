f83e569a10deab118efb90402b25130e248150d3
Glitch (buttered-successful-bull) <none>
2021-03-10 14:50:00 +0000

    🍩🚇 Checkpoint ./logger.js:23585414/262008 ./server.js:23585414/11

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const discord = require("discord.js");
 const client = new discord.Client({
   disableEveryone: false
 });
-//require("./bot.js");
+require("./logger.js");
 require("./uptime.js");
 client.commands = new discord.Collection();
 client.aliases = new discord.Collection();
