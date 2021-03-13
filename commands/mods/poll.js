40018cfc6d05d6c99e3cb44c07fd078e45b5acc3
Glitch (buttered-successful-bull) <none>
2021-03-13 14:17:41 +0000

    🍽💾 Checkpoint ./imdb.js:23585414/1296 ./commands/extras/imdb.js:23585414/5664 ./poll.js:23585414/1123 ./commands/mods/poll.js:23585414/2237

---
+++ commands/mods/poll.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+const Poll_Emoji_1 = "👍";
+const Poll_Emoji_2 = "👎";
+const { prefix } = require("../../config.json");
+
+module.exports = {
+  name: "poll",
+  description: "Creates A Simple Poll",
+  usage: `\`${prefix}poll <Topic>\``,
+  async run(client, message, args) {
+    if (!message.guild.me.permissions.has("EMBED_LINKS"))
+      return message.channel.send(
+        `I Don't Have Permission To Use This Command! Embed Links`
+      );
+    if (!message.member.permissions.has("ADMINISTRATOR"))
+      return message.channel.send(
+        `You Don't Have Permission To Use This Command! Administrator`
+      );
+    if (!args[0]) return message.channel.send(`Usage: ${this.usage}`);
+
+    let Message = args.slice(0).join(" ");
+    let Poll = await message.channel.send(
+      new Discord.MessageEmbed()
+        .setColor(`RANDOM`)
+        .setDescription(`📋 ${Message}`)
+        .setFooter(`Poll Created By : ${message.author.username} ${`)
+    );
+
+    await Poll.react(`${Poll_Emoji_1}`);
+    await Poll.react(`${Poll_Emoji_2}`);
+    await message.delete();
+  }
+};
