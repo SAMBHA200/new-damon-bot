9a193fb46337ac5f246fd7105ef3680a6f78c011
Glitch (buttered-successful-bull) <none>
2021-03-15 19:33:52 +0000

    🐜🍗 Checkpoint ./commands/useful/test.js:23585414/3840

--- commands/useful/test.js
+++ commands/useful/test.js
@@ -6,7 +6,20 @@ module.exports = {
   usage: "reply <message> or say <message>",
 
   async run(client, message, args) {
-    let msg = message.channel.send(`Changed`)
-      .then(msg => {
-        msg.edit("hello"+{ timeout: 10000 });
-  })}
+    message.channel
+      .send(`Loading..`)
+      .then(message => {
+        setTimeout(function() {
+          message.edit(`Loading.. 1`);
+        }, 10000);
+      })
+      .then(message => {
+        setTimeout(function() {
+          message.edit(`Loading... 2`);
+        }, 10000);
+      });
+    //.then(msg => {
+    //   msg.edit({ timeout: 10000, msg: "hello success" });
+    //  });
+  }
+};
