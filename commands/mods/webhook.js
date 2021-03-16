8a89de91d89429967bbd7612998e9776d560199a
Glitch (buttered-successful-bull) <none>
2021-03-16 18:44:45 +0000

    ❣️📙 Checkpoint ./commands/mods/webhook.js:23585414/5745

---
+++ commands/mods/webhook.js
@@ -0,0 +1,20 @@
+const discord = require("discord.js");
+const embed = new discord.MessageEmbed();
+
+module.exports = {
+  name: "streaming",
+  description: "",
+  async run(client, message, args) {
+    if (!message.author.id === bowner)
+      return message.reply("Owners Only Commamd");
+    const activity = args.join(" ");
+    client.user.setActivity(activity, {
+      type: "STREAMING",
+      url: "https://discord.gg/wXemeVm"
+    });
+    message.delete();
+    await message.channel.send(`Set Activity PLAYING ${activity}`).then(msg => {
+      msg.delete({ timeout: 10000 });
+    });
+  }
+};
