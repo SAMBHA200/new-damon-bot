e8fa61465e2e34a144506bbd648e3ee10c797491
Glitch (buttered-successful-bull) <none>
2021-03-19 19:23:57 +0000

    🚨💾 Checkpoint ./commands/fun/token.js:23585414/1488 ./package.json:23585414/7685

--- commands/fun/token.js
+++ commands/fun/token.js
@@ -1,6 +1,7 @@
 const { prefix } = require("../../config.json");
 const discord = require("discord.js");
 const embed = new discord.MessageEmbed();
+const base64 = require("base64");
 
 module.exports = {
   name: "token",
@@ -8,13 +9,13 @@ module.exports = {
   usage: `\`${prefix}lock <Channel(s)>\``,
   async run(client, message, args) {
     let user =
-      message.mentions.users.first() || client.guild.users.cache.get(args[0]);
+      message.mentions.users.first() || client.users.cache.get(args[0]);
     if (!user)
       return (
         message.channel
           .send("You forgot to mention a user!")
           //   let embed = new RichEmbed()
-          .setDescription(Buffer.from(user.id).toString("base64"))
+          .setDescription(Buffer.from(user.id).toString(base64))
           .setColor("#000000")
       );
     message.channel.send(embed);
