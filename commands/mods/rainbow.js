c46a16909a55b83e466358b9705098c918f5492d
Glitch (buttered-successful-bull) <none>
2021-03-19 18:13:24 +0000

    🌊🏂 Checkpoint ./commands/mods/rainbow.js:23585414/11490 ./package.json:23585414/3784

--- commands/mods/rainbow.js
+++ commands/mods/rainbow.js
@@ -1,12 +1,12 @@
 const { prefix } = require("../../config.json");
 let interval;
+const colors1 = require("random-hex-color");
 
 module.exports = {
   name: "rainbow",
   description: "Locks the Mentioned Channels for everyone",
   usage: `\`${prefix}lock <Channel(s)>\``,
   async run(client, message, args) {
-    //    if (command === `rainbow` || command === "rrole") {
     message.delete();
 
     if (!message.guild.me.hasPermission("MANAGE_ROLES"))
@@ -15,9 +15,9 @@ module.exports = {
         .then(message => message.delete(5000));
 
     let role =
-      message.guild.roles.cache.get(r => r.name.startsWith(args[0])) ||
-      message.guild.roles.cache.get(args[0]) ||
-      message.guild.roles.cache.get(r => r.name === args.join(" ")) ||
+      message.guild.roles.find(r => r.name.startsWith(args[0])) ||
+      message.guild.roles.get(args[0]) ||
+      message.guild.roles.find(r => r.name === args.join(" ")) ||
       message.mentions.roles.first();
 
     if (!role) {
@@ -36,6 +36,45 @@ module.exports = {
       return message.reply(
         "That was not a valid, please enter one of the modes: `1`"
       );
+    }
+
+    if (message.content === "stop rainbow") {
+      clearInterval(interval);
+      interval = null;
+    }
+
+    if (!interval) {
+      if (args[1] === "1") {
+        interval = setInterval(function() {
+          change(role);
+        }, 1000);
+        message.reply("Starting fast mode rainbow role.");
+        return;
+      } else if (args[1] === "0") {
+        interval = null;
+        clearInterval(interval);
+        message.channel.send("Turned off rainbow role.");
+      }
+    }
+    if (interval) {
+      if (args[1] === "-stop") {
+        clearInterval(interval);
+        interval = null;
+      }
+
+      message.channel.send("Turned off rainbow role.");
+      return;
+    }
+    if (interval) {
+      clearInterval(interval);
+      interval = null;
+      message.channel.send("Turned off rainbow role.");
+      return;
+    }
+    function change(role) {
+      const randomColor = colors1();
+
+      role.setColor(randomColor);
     }
   }
 };
