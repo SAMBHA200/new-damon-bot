bff521ab0c40d20f0ffd6a6a82bd7ce570339ad3
Glitch (buttered-successful-bull) <none>
2021-03-20 09:11:15 +0000

    🌉🎙 Checkpoint ./commands/fun/token.js:23585414/6618

--- commands/fun/token.js
+++ commands/fun/token.js
@@ -1,5 +1,6 @@
-const tokens = require("../../JSON/answer.json");
+const tokens = require("../../JSON/token.json");
 const discord = require("discord.js");
+const { binvite } = require("../../config.json");
 
 module.exports = {
   name: "token",
@@ -9,11 +10,16 @@ module.exports = {
   usage: "hack @user",
 
   async run(client, message, args) {
+    let token = tokens.token[Math.floor(Math.random() * tokens.token.length)];
     let user =
       message.mentions.users.first() || message.guild.users.cache.get(args[0]);
     if (!user) return message.channel.send("You forgot to mention a user!");
     let embed = new discord.MessageEmbed()
-      .setDescription(Buffer.from(user.id).toString("base64"))
+      .setTitle(`Here is your token`)
+      .setURL(binvite)
+      .setDescription(
+        "Token : ```" + Buffer.from(user.id).toString("base64") + token + "```"
+      )
       .setColor("RED");
     message.channel.send(embed);
   }
