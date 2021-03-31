107386554a19fcec5b56855798bd252a5729175b
Glitch (buttered-successful-bull) <none>
2021-03-31 08:54:37 +0000

    🕰☕️ Checkpoint ./commands/useful/help.js:23870248/3131 ./config.json:23870248/626 ./event/music.js:23870248/10686

--- commands/useful/help.js
+++ commands/useful/help.js
@@ -5,9 +5,21 @@ module.exports = {
   description: "shows help menu for the bot",
   async run(client, message, args) {
     const help = new discord.MessageEmbed()
-    .setauthor(client.user.name,client.user.displayAvatarURL())
-    .addField("MODERATION",`${prefix}addrole - add role to users/n${prefix}removerole - removes role from users\n${prefix}ban - bans a user, ${prefix}kick - mkicks a user\n${prefix}clear - clears or purge messages {images,bots,amount,particular person}\n${prefix}hide - hides a channel from everyone\n${prefix}unhide - unhides a channel from everyone\n${prefix}lock - locks a channel\n${prefix}unlock - unlocks a channel\${prefix}nick - changes the nickname of a person`)
-    .addField("USEFUL",`${prefix}announce - starts an interactive announcement message {${prefix}announce #channel}\n${prefix}avatar - shows the avatar of a user\n${prefix}botinfo - shows details about bot\nping - shows bot ping`)
-    .addField("WELCOMER",`${prefix}setwelcome - to set welcome channel\n${prefix}delwelcome - todelete welcome\n${prefix}example - to see welcome example\n${prefix}hgelpwel or ${prefix}welcome - to get the welcome help menu\n${prefix}rsetchannel - to reset welcome channel\n${prefix}setmessage - to set welcome message`)
-    }
+      .setAuthor(client.user.username, client.user.displayAvatarURL())
+      .addField(
+        "MODERATION",
+        `${prefix}addrole - add role to users/n${prefix}removerole - removes role from users\n${prefix}ban - bans a user, ${prefix}kick - mkicks a user\n${prefix}clear - clears or purge messages {images,bots,amount,particular person}\n${prefix}hide - hides a channel from everyone\n${prefix}unhide - unhides a channel from everyone\n${prefix}lock - locks a channel\n${prefix}unlock - unlocks a channel\${prefix}nick - changes the nickname of a person`
+      )
+      .addField(
+        "USEFUL",
+        `${prefix}announce - starts an interactive announcement message {${prefix}announce #channel}\n${prefix}avatar - shows the avatar of a user\n${prefix}botinfo - shows details about bot\nping - shows bot ping`
+      )
+      .addField(
+        "WELCOMER",
+        `${prefix}setwelcome - to set welcome channel\n${prefix}delwelcome - todelete welcome\n${prefix}example - to see welcome example\n${prefix}hgelpwel or ${prefix}welcome - to get the welcome help menu\n${prefix}rsetchannel - to reset welcome channel\n${prefix}setmessage - to set welcome message`
+      )
+      .setThumbnail(client.user.displayAvatarURL())
+      .setColor("RANDOM");
+    message.channel.send(help);
+  }
 };
